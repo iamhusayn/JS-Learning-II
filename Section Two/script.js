2525f68dbcbaa39f443484b652f6a3e00afc50f6
Husayn <khaybabz@gmail.com>
2025-05-15 16:37:47 +0100

    More on Functions

--- Section Two/script.js
+++ Section Two/script.js
@@ -6,7 +6,7 @@
 // if (passTest) withDriversLicense = true;
 // if (withDriversLicense) console.log("Strap your seatbelt");
 
-// const interface = "Windows";
+// const driver = "Windows";
 
 //FUNCTIONS
 function logger() {
@@ -40,6 +40,7 @@ console.log(fruitProcessor(2, 10));
 //Function Vs Expression
 //This is called a function Declaration
 //It can be calledd before they are defined.
+
 const ageA = family(1990);
 
 function family(birthYear) {
@@ -70,10 +71,37 @@ console.log(retirementAge(1995, "Bowale"));
 
 //Calling a function inside a function
 
-function food(pepper, maggi, salt) {
-  const friedRice = `I made a fried rice using ${pepper} pepper, ${maggi} maggi and ${salt} pinch of salt.`;
-  return friedRice;
-}
+// function diceIngredient(ingredients) {
+//   return ingredients * 4;
+// }
 
-const cook = food(3, 1, 2);
-console.log(cook);
+// function prepare(pepper, maggi, salt) {
+//   const pepperPieces = diceIngredient(pepper);
+//   const maggiPieces = diceIngredient(maggi);
+//   const saltPieces = diceIngredient(salt);
+
+//   const friedRice = `I made fried rice using ${pepperPieces} pepper, ${maggiPieces} maggi and ${saltPieces} pinch of salt.`;
+//   return friedRice;
+// }
+
+// console.log(prepare(3, 2, 1)); //This will return undefined because of the return statement in the diceIngredient function.
+
+//Practice
+const realAge = (bYear) => 2050 - bYear;
+
+const yearUntilMarriage = function (bYear, firstName) {
+  const nowAge = realAge(bYear);
+  const dueAge = 60 - nowAge;
+
+  if (dueAge > 0) {
+    console.log(
+      `Meet ${firstName}, he is due to marry in ${dueAge} years from now`
+    );
+    return dueAge;
+  } else {
+    console.log(`Meet ${firstName}, he is already married`);
+    return -1;
+  }
+};
+console.log(yearUntilMarriage(1990, "John"));
+console.log(yearUntilMarriage(1985, "Jane"));
